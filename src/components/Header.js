86235b864f28f7b1609bdd8bff0d0a20ae4eb877
abi4679553 <abivengadajalam7708@gmail.com>
2026-06-23 15:54:36 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+
+  return (
+    <div className='flex bg-orange-50 '>
+      <div>
+        <p className='font-thin '>Sweet Crumb Factory</p>
+      </div>
+      <div>
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Service</p>
+        <p>Contact</p>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Header
