d0d7ab41d0d2d31c4890a9e079f8995b9341134c
abi4679553 <abivengadajalam7708@gmail.com>
2026-06-24 12:06:21 +0530

    Header creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,20 @@ import React from 'react'
 function Header() {
 
   return (
-    <div className='flex bg-orange-50 '>
-      <div>
-        <p className='font-thin '>Sweet Crumb Factory</p>
+    <div className='bg-[#F7F1E8] flex flex-wrap  justify-between  gap-5 p-5 '>
+      <div className='flex text-3xl gap-2 ml-20 font-bold'>
+        <p className='text-[#3A2215]'>Sweet Crumb </p>
+        <p className='text-[#D97A42]'>Factory</p>
       </div>
-      <div>
-        <p>Home</p>
-        <p>About Us</p>
-        <p>Service</p>
-        <p>Contact</p>
+      <div className='flex flex-wrap  gap-10 ml-[35%] text-xl  justify-between mt-4'>
+        <p>HOME</p>
+        <p>ABOUT </p>
+        <p>CONTACT</p>
+        
       </div>
+       <div>
+       <button className='bg-[#4A2200] text-[#FFFFFF] p-4 px-5 mr-32 '>ORDER NOW</button>
+      </div> 
 
 
     </div>
